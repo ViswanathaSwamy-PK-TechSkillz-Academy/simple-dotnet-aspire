0102f8c7e6066c12f80a7ec081b4b72ce344151d
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-06-27 19:27:11 +0530

    Changes

--- Aspire.StarterApp/Aspire.StarterApp.ApiService/Program.cs
+++ Aspire.StarterApp/Aspire.StarterApp.ApiService/Program.cs
@@ -6,7 +6,7 @@ builder.AddServiceDefaults();
 // Add services to the container.
 builder.Services.AddProblemDetails();
 
-var sqlDbConnectionString = builder.Configuration.GetConnectionString("SqlDbConnectionString") ?? string.Empty;
+var sqlDbConnectionString = builder.Configuration.GetConnectionString("SqlDbConnectionString") ?? "ConnectionStringNotFound";
 Console.WriteLine($"Connection String: {sqlDbConnectionString}");
 
 var app = builder.Build();
