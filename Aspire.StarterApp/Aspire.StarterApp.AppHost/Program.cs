5d2b413b2b708d24d84f819305137a66142d613a
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-06-28 12:34:36 +0530

    Changes

--- Aspire.StarterApp/Aspire.StarterApp.AppHost/Program.cs
+++ Aspire.StarterApp/Aspire.StarterApp.AppHost/Program.cs
@@ -2,9 +2,10 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var cache = builder.AddRedis("cache");
 
-var sqldbConnectionString = builder.AddConnectionString("SqlDbConnectionString");
+var sqldbConnectionString = builder.AddConnectionString("SqlDbConnStr");
 
 var apiService = builder.AddProject<Projects.Aspire_StarterApp_ApiService>("apiservice")
+    .WithReference(cache)
     .WithReference(sqldbConnectionString);
 
 builder.AddProject<Projects.Aspire_StarterApp_Web>("webfrontend")
